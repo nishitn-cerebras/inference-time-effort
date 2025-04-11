8991d09ecbeced6b3c7b3e2ad9044c3abd88cda2
nishitn-cerebras <nishit.neema@cerebras.net>
2025-04-11 03:26:33 -0700

    S2_10_executions script updated for mlftmp

--- cepo/S2/S2_10_executions_per_problem.py
+++ cepo/S2/S2_10_executions_per_problem.py
@@ -52,9 +52,9 @@ async def rate_and_analyse_final_cepo_response_async(problem_number, question, f
                 )
                 sys.exit(1)
 
-async def process_batch(batch_data, batch_number, output_dir, problem_ids_file, client, model):
-    """Processes a batch of 20 examples asynchronously."""
-    print(f"Processing batch {batch_number} with {len(batch_data)} examples")
+async def process_batch(batch_data, problem_number, output_dir, problem_ids_file, client, model):
+    """Processes a batch of examples asynchronously."""
+    print(f"Processing batch {problem_number} with {len(batch_data)} examples")
     tasks = [
         rate_and_analyse_final_cepo_response_async(*data, client, model)
         for data in batch_data
@@ -63,7 +63,7 @@ async def process_batch(batch_data, batch_number, output_dir, problem_ids_file, 
     results = await asyncio.gather(*tasks)
 
     # Write batch results
-    batch_filename = os.path.join(output_dir, f"problem_{batch_number}.jsonl")
+    batch_filename = os.path.join(output_dir, f"problem_{problem_number}.jsonl")
     with open(batch_filename, "w", encoding="utf-8") as f:
         for entry in results:
             f.write(json.dumps(entry) + "\n")
@@ -72,27 +72,37 @@ async def process_batch(batch_data, batch_number, output_dir, problem_ids_file, 
     with open(problem_ids_file, "a", encoding="utf-8") as f:
         f.write("\n".join([data[0] for data in batch_data]) + "\n")
 
-    print(f"Batch {batch_number} written to {batch_filename}")
+    print(f"Problem {problem_number} written to {batch_filename}")
 
 async def main():
     """Main function that processes all files in parallel batches."""
-    # # vllm: Llama3.3 70B
-    # api_key = "serving-on-vllm"
-    # base_url = "http://127.0.0.1:8066/v1"
-    # model = "meta-llama/Llama-3.3-70B-Instruct"
+    # vllm: Llama3.3 70B
+    api_key = "serving-on-vllm"
+    base_url = "http://127.0.0.1:8066/v1"
+    model = "meta-llama/Llama-3.3-70B-Instruct"
 
-    # Gemini flash 2.0
-    # api_key = "AIzaSyBuR0Cqa87PMfq1pairtCc1dsFFad0BC7k"
-    api_key = "AIzaSyBnIV08hEWduNDnXqcmPWydARhtpJW5zE8"
-    base_url = "https://generativelanguage.googleapis.com/v1beta/openai/"
-    model = "gemini-2.0-flash"
+    # # # Gemini flash 2.0
+    # # api_key = "AIzaSyBuR0Cqa87PMfq1pairtCc1dsFFad0BC7k"
+    # # # api_key = "AIzaSyBnIV08hEWduNDnXqcmPWydARhtpJW5zE8"
+    # # base_url = "https://generativelanguage.googleapis.com/v1beta/openai/"
+    # # model = "gemini-2.0-flash"
+
+    # # # Together API call
+    # # api_key="d8762e8d418b93a47f7cb11bdbb952b9cf5b3c0b4948b38fcfe70445c255ae1b",
+    # # base_url="https://api.together.xyz/v1"
+    # # model = "meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo"
+
+    # # Quasur alpha API call
+    # api_key = "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"
+    # base_url="https://openrouter.ai/api/v1"
+    # model="nvidia/llama-3.3-nemotron-super-49b-v1:free"
 
     # OpenAI Async Client
     client = AsyncOpenAI(api_key=api_key, base_url=base_url)
 
     plan_dir = "/mlf-transfers-only/harshg/inference-time-effort/NuminaMath_Analysis/generated_plans"
     execution_dir = "/mlf-transfers-only/harshg/inference-time-effort/NuminaMath_Analysis/executed_plans11"
-    output_dir = "/mlf-transfers-only/nishitn/inference_time_effort/cepo/NuminaMath/gemini_generated_s2_data_10_executions_per_problem/"
+    output_dir = "data/llama_generated_s2_data_10_executions_per_problem/"
     problem_ids_file = os.path.join(output_dir, "processed_problem_ids.txt")
 
     if not os.path.exists(output_dir):
@@ -111,17 +121,6 @@ async def main():
             processed_problem_ids = set(f.read().splitlines())
 
     batch_data = []
-
-    # Identify last processed batch number
-    existing_batches = glob.glob(os.path.join(output_dir, "batch_*.jsonl"))
-    batch_numbers = [
-        int(re.search(r"batch_(\d+).json", os.path.basename(f)).group(1))
-        for f in existing_batches if re.search(r"batch_(\d+).json", os.path.basename(f))
-    ]
-
-    # Start from the next batch number
-    batch_number = max(batch_numbers, default=0) + 1
-    print(f"Starting from batch number {batch_number}")
 
     files = glob.glob(os.path.join(plan_dir, "*.json"))
     print(f"Total files: {len(files)}")
@@ -159,7 +158,7 @@ async def main():
             question = question_text.split(prefix, 1)[1].strip() if prefix in question_text else ""
             ground_truth = data['answer']
 
-            # check if execution  directory for that problem contain any file or not
+            # check if execution directory for that problem contain any file or not
             temp_execution_dir = os.path.join(execution_dir, f"problem_{problem_number}")
             if not os.listdir(temp_execution_dir):
                 problems_with_no_execution += 1
@@ -193,16 +192,11 @@ async def main():
                     break
 
             if len(batch_data) == 10:
-                await process_batch(batch_data, batch_number, output_dir, problem_ids_file, client, model)
+                await process_batch(batch_data, problem_number, output_dir, problem_ids_file, client, model)
             else:
                 plans_with_less_execution += 1
 
             batch_data = []
-            batch_number += 1  # Increment for next batch
-
-    # Process remaining batch if less than batch_size
-    if batch_data:
-        await process_batch(batch_data, batch_number, output_dir, problem_ids_file, client, model)
 
     print(f"Total plans with less execution: {plans_with_less_execution}")
     print(f"Total problems with no execution: {problems_with_no_execution}")
